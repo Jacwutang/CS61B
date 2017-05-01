b40d67378eb7d9184f1f6b162ccc78b32f3e9196
Jack Wu <jackwu350@yahoo.com>
2017-04-30 22:18:50 -0700

    Read universe radius from file input

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,3 +1,19 @@
 public class NBody {
 
+    public static double readRadius(String PlanetsTextFile){
+
+        In in = new In(PlanetsTextFile);
+
+        int n = in.readInt();
+
+        double radius = in.readDouble();
+
+
+
+
+        return radius;
+    }
+
+
+
 }
